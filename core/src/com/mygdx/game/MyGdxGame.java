3609d2b947347a6eba8ba3f15ee82f5773316c34
Zpiralh <elias.wirtanen@gmail.com>
2017-11-11 23:42:09 +0200

    Update MyGdxGame.java

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,19 +5,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.mygdx.game.states.GameStateManager;
 import com.mygdx.game.states.MenuState;
 
 public class MyGdxGame extends ApplicationAdapter {
 	private GameStateManager gsm;
 	private SpriteBatch batch;
-	
+	private OrthogonalTiledMapRenderer tmr;
+	private TiledMap map;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
 		Gdx.gl.glClearColor(1,0,0,1);
 		gsm.push(new MenuState(gsm));
+		map = new TmxMapLoader().load("testi.tmx");
+		tmr = new OrthogonalTiledMapRenderer(map);
 	}
 
 	@Override
@@ -26,10 +33,12 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
+		tmr.render();
 	}
-	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		tmr.dispose();
+		map.dispose();
 	}
 }
