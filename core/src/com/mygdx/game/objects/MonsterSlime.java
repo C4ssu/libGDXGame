240621d5b34cc3a4274e9b2adbd1a242588cdebe
Zpiralh <elias.wirtanen@gmail.com>
2017-11-12 19:15:04 +0200

    Create MonsterSlime.java

---
+++ core/src/com/mygdx/game/objects/MonsterSlime.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.objects;
+
+/**
+ * Created by Elias on 12.11.2017.
+ */
+
+class MonsterSlime extends Monster {
+    public MonsterSlime(int id, double speed, int health) {
+        super(id, speed, health);
+    }
+}
